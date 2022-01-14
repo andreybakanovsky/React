cf5ec7e4602e6e67406226527bb47906da9042c1
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-14 16:00:04 +0300

    3h course - add a component

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -1,28 +1,16 @@
 import React, { useState } from 'react';
+import Counter from './components/counter';
 
 function App() {
-  const [stars, setStars] = useState(0); // it's hook
-  const [value, setValue ] = useState('Text in an inpit'); // it's hook
-
-  function increment() {
-    setStars(stars + 1);
-  }
-
-  function decrement() {
-    setStars(stars - 1);
-  }
+  const [value, setValue] = useState('Text in an inpit'); // it's hook
 
   return (
     <div className="App">
-      <h1> Stars:  {stars} </h1>
-      <h1> {value} </h1>
-      <input 
-      type="text"
-       value={value}
-       onChange={event => setValue(event.target.value)} // двухстороннее связывание
-       />
-      <button onClick={increment}> Increment </button>
-      <button onClick={decrement}> Decrement </button>
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
   );
 }
