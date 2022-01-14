2a8acdd9117af5ee7d1226b119da1210dd57e2bb
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-14 15:52:08 +0300

    3h course - start

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -1,11 +1,31 @@
 import React, { useState } from 'react';
 
 function App() {
+  const [stars, setStars] = useState(0); // it's hook
+  const [value, setValue ] = useState('Text in an inpit'); // it's hook
+
+  function increment() {
+    setStars(stars + 1);
+  }
+
+  function decrement() {
+    setStars(stars - 1);
+  }
+
   return (
     <div className="App">
-
+      <h1> Stars:  {stars} </h1>
+      <h1> {value} </h1>
+      <input 
+      type="text"
+       value={value}
+       onChange={event => setValue(event.target.value)} // двухстороннее связывание
+       />
+      <button onClick={increment}> Increment </button>
+      <button onClick={decrement}> Decrement </button>
     </div>
   );
 }
 
 export default App;
+
