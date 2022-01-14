db0f9d22eedbaedd9e4ebe8205cd12124b8d052f
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-14 18:39:40 +0300

    3h course - add a post with styles

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
-
+import './styles/App.css';
 function App() {
   const [value, setValue] = useState('Text in an inpit'); // it's hook
 
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />
+
+      <div className="post" >
+        <div className='post__content'>
+          <strong>1. Javascript </strong>
+          <div>
+            Javascript - programming language
+          </div>
+        </div>
+        <div className='post__btns'>
+          <button>Delete</button>
+        </div>
+      </div>
+
     </div>
   );
 }
