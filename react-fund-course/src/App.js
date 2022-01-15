845ee43811e328485b69b4734c5eecc017f71802
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-15 15:11:20 +0300

    3h course - create component PostList

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
 import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 import './styles/App.css';
+
 function App() {
-  
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ])
+
+  const [posts2, setPosts2] = useState([
+    { id: 1, title: 'Python', body: 'Description' },
+    { id: 2, title: 'Python 2', body: 'Description' },
+    { id: 3, title: 'Python 3', body: 'Description' },
+  ])
+
   return (
     <div className="App">
-      <PostItem post={{ id: 1, title: 'Javascript', body: 'Desctition' }} />
-      <PostItem post={{ id: 2, title: 'Javascript', body: 'Desctition' }} />
-      <PostItem post={{ id: 3, title: 'Javascript', body: 'Desctition' }} />
-      <PostItem post={{ id: 4, title: 'Javascript', body: 'Desctition' }} />
-
+      <PostList posts={posts} title= "Posts about JS" />
+      <PostList posts={posts2} title= "Posts about Pyton" />
     </div>
   );
 }
