72355e64cca099c12e8360b89ea09438c4b0a8d2
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-15 22:33:15 +0300

    3h course - add the bottons Delete functional

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -19,10 +19,14 @@ function App() {
     setPosts([...posts, newPost]);
   }
 
+  function removePost(post) {
+    setPosts(posts.filter(p => p.id !== post.id));
+  }
+
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <PostList posts={posts} title="Posts about JS" />
+      <PostList remove={removePost} posts={posts} title="Posts about JS" />
     </div>
   );
 }
--- react-fund-course/src/components/PostList.jsx
+++ react-fund-course/src/components/PostList.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import PostItem from './PostItem';
 
-const PostList = ({ posts, title }) => {
+const PostList = ({ posts, title, remove }) => {
     return (
         <div>
             <h1 style={{ textAlign: 'center' }}>
-               {title}
+                {title}
             </h1>
             {posts.map((post, index) => // передадим номер элемента в массиве
-                <PostItem number={index + 1} post={post} key={post.id} />
+                <PostItem remove={remove} number={index + 1} post={post} key={post.id} />
             )}
         </div>
     )
