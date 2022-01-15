00ad64bc2ff5f7ea93cfcbbfa4fc1a8405d5e80e
andrey bakanovsky <andrey.bakanovsky@outlook.com>
2022-01-15 17:23:21 +0300

    3h course - create components MyButton, MyInput

--- react-fund-course/src/App.js
+++ react-fund-course/src/App.js
@@ -3,6 +3,8 @@ import ClassCounter from './components/ClassCounter';
 import Counter from './components/counter';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 import './styles/App.css';
 
 function App() {
@@ -12,16 +14,14 @@ function App() {
     { id: 3, title: 'Javascript 3', body: 'Description' },
   ])
 
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' },
-  ])
-
   return (
     <div className="App">
-      <PostList posts={posts} title= "Posts about JS" />
-      <PostList posts={posts2} title= "Posts about Pyton" />
+      <form>
+        <MyInput type="text" placeholder='Posts name' />
+        <MyInput type="text" placeholder='Posts description ' />
+        <MyButton disabled >Create post</MyButton>
+      </form>
+      <PostList posts={posts} title="Posts about JS" />
     </div>
   );
 }
